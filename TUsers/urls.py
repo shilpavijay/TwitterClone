d6c1aa3d9ba9377dc7ac31020fd0f5893cda140d
Shilpa AB <shilpa.amberker@gmail.com>
2020-12-10 22:00:50 +0530

    User module refactoring

--- TUsers/urls.py
+++ TUsers/urls.py
@@ -4,12 +4,11 @@ from rest_framework import routers, serializers, viewsets
 
 urlpatterns = [
     path('signup', views.AccountSignup),
-    path('users',views.users),
     path('<str:username>/account', views.AccountUpdate),
     path('auth', views.GetToken),
-    path('login', views.Login),
     path('<str:loggedin_user>/<str:user>/follow', views.FollowUser),
     path('<str:username>/followers', views.GetFollowers),
     path('<str:username>/following', views.GetFollowing),
     path('<str:username>/block', views.Block_user), 
+    path('users',views.users),
 ]
--- TUsers/views.py
+++ TUsers/views.py
@@ -23,6 +23,13 @@ def _get_token(request=None):
 
 @api_view(['POST'])
 def GetToken(request):
+    '''
+    Purpose: Get Access token
+    Input: 
+    username (mandatory) <str> Account user 
+    password (mandatory) <str> Password
+    Output: Token that expires in 5 minutes
+    '''
     user = request.query_params.get('username',None)
     pwd = request.query_params.get('password',None)
     try:
@@ -35,42 +42,63 @@ def GetToken(request):
             except Exception as e:
                 raise e
         else:
-            resp = {'Authentication Failed. Wrong Username/Password'}
+            resp = {"Error": 'Authentication Failed. Wrong Username/Password'}
             return Response(resp, status=status.HTTP_403_FORBIDDEN)
-    except:
-        return Response("Please provide a valid Username and Password",status=status.HTTP_400_BAD_REQUEST)   
+    except Exception as e:
+        error = {'Error_code': status.HTTP_400_BAD_REQUEST,
+                        'Error_Message': "Please provide a valid Username and Password"}
+        logger.error(e) 
+        return Response(error,status=status.HTTP_400_BAD_REQUEST)   
 
 @api_view(['POST'])
 def Login(request):
+    '''
+    Purpose: Login to the Application
+    Input: 
+    token (mandatory) <str> user token 
+    Output: User object of the logged in user
+    '''
     token = _get_token(request)
     try:
         payload = jwt.decode(token,settings.AUTH_TOKEN)
         user = TUser.objects.get(username = payload.get('username'))
         serializer = TUserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
     except (jwt.ExpiredSignature, jwt.DecodeError, jwt.InvalidTokenError):
-        return Response("Error: Token is Invalid", status=status.HTTP_403_FORBIDDEN)
-    except:
-        return Response("Error: Internal Server Error", status=status.HTTP_403_FORBIDDEN)
-    return Response(serializer.data, status=status.HTTP_201_CREATED)
+        error = {'Error_code': status.HTTP_403_FORBIDDEN,
+                        'Error_Message': "Token is Invalid"}
+        logger.error(e) 
+        return Response(error,status=status.HTTP_403_FORBIDDEN) 
+    except Exception as e:
+        error = {'Error_code': status.HTTP_403_FORBIDDEN,
+                        'Error_Message': "Internal Server Error"}
+        logger.error(e) 
+        return Response(error,status=status.HTTP_403_FORBIDDEN) 
     
-
 @api_view(['POST'])
 def AccountSignup(request):
+    '''
+    Purpose: Create a new user
+    Input: 
+    username (mandatory) <str> Chosen Username 
+    password (mandatory) <str> Chosen Password
+    Output: User object of the created user
+    '''
     serializer = TUserSerializer(data=request.query_params)
     if serializer.is_valid():
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
     return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET'])
-def users(request):
-    if request.method == 'GET':
-        users = TUser.objects.all()
-        serializer = TUserSerializer(users, many=True)
-        return Response(serializer.data)
         
 @api_view(['PUT'])
 def AccountUpdate(request,username):
+    '''
+    Purpose: Update Account Details
+    Input: -
+    username (mandatory) <str> Chosen Username 
+    country (optional) <str> Country
+    Output: User object of the updated user
+    '''
     try:
         query_dict = QueryDict('', mutable=True)
         query_dict.update({"username": username})
@@ -80,46 +108,93 @@ def AccountUpdate(request,username):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
-    except:
-        return Response("Invalid details", status=status.HTTP_400_BAD_REQUEST)
+    except Exception as e:
+        error = {'Error_code': status.HTTP_400_BAD_REQUEST,
+                        'Error_Message': "Invalid details"}
+        logger.error(e)
+        return Response(error, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['PUT'])
 def FollowUser(request,loggedin_user,user):
+    '''
+    Purpose: Follow the user
+    Input: -
+    Output: User object of the logged in user
+    '''
     try:
         cur_user=TUser.objects.get(username=loggedin_user)
         fol_user=TUser.objects.get(username=user)
         cur_user.following.add(fol_user)
         cur_user.save()
-        return Response("Follow Request Updated", status=status.HTTP_204_NO_CONTENT)
-    except:
-        return Response("Request Failed. Invalid Details", status=status.HTTP_400_BAD_REQUEST)    
+        serializer = TUserSerializer(cur_user)
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+    except Exception as e:
+        error = {'Error_code': status.HTTP_400_BAD_REQUEST,
+                        'Error_Message': "Request Failed. Invalid Details"}
+        logger.error(e)
+        return Response(error, status=status.HTTP_400_BAD_REQUEST)  
 
 @api_view(['GET'])
 def GetFollowers(request,username):
+    '''
+    Purpose: Get all the followers for the user
+    Input: -
+    Output: User object of all the following users
+    '''
     try:
         user = TUser.objects.get(username=username)
         followers = user.followers.all()
         serializer = TUserSerializer(followers, many=True)
         return Response(serializer.data)
-    except:
-        return Response("User does not exist!", status=status.HTTP_400_BAD_REQUEST)  
+    except Exception as e:
+        error = {'Error_code': status.HTTP_400_BAD_REQUEST,
+                        'Error_Message': "User does not exist"}
+        logger.error(e)
+        return Response(error, status=status.HTTP_400_BAD_REQUEST) 
 
 @api_view(['GET'])
 def GetFollowing(request,username):
+    '''
+    Purpose: Get all the users the given user is following
+    Input: -
+    Output: User object of all the followed users
+    '''
     try:
         user = TUser.objects.get(username=username)
         following = user.following.all()
         serializer = TUserSerializer(following, many=True)
         return Response(serializer.data)
-    except:
-        return Response("User does not exist!", status=status.HTTP_400_BAD_REQUEST)  
+    except Exception as e:
+        error = {'Error_code': status.HTTP_400_BAD_REQUEST,
+                        'Error_Message': "User does not exist"}
+        logger.error(e)
+        return Response(error, status=status.HTTP_400_BAD_REQUEST) 
 
 @api_view(['PUT'])
 def Block_user(request,username):
+    '''
+    Purpose: Block the user
+    Input: -
+    Output: Blocked user
+    '''
     try:
         user = TUser.objects.get(username=username)
         user.blocked = True
         user.save()
-        return Response("User Blocked", status=status.HTTP_204_NO_CONTENT)
-    except:
-        return Response("User Does not exist!", status=status.HTTP_400_BAD_REQUEST) 
+        serializer = TUserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_204_NO_CONTENT)
+    except Exception as e:
+        error = {'Error_code': status.HTTP_400_BAD_REQUEST,
+                        'Error_Message': "User does not exist"}
+        logger.error(e)
+        return Response(error, status=status.HTTP_400_BAD_REQUEST) 
+
+@api_view(['GET'])
+def users(request):
+    '''
+    Debugging
+    '''
+    if request.method == 'GET':
+        users = TUser.objects.all()
+        serializer = TUserSerializer(users, many=True)
+        return Response(serializer.data)        
